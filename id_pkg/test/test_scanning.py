d9a6d3ed74d0e30d887b3411ce615fa0e4211574
jamorgan1 <jamorgan1@aggies.ncat.edu>
2021-03-24 20:37:10 -0400

    Added test file for scanning, test passed.

---
+++ id_pkg/test/test_scanning.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestScanning(unittest.TestCase):
+    def test_scanning_stub(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
